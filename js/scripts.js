557fdf24126c5902ad6c512c4bb96bd66fc409e1
Matthew <106117475+MLindbloom@users.noreply.github.com>
2023-10-02 21:41:47 -0700

    Update scripts.js
    moved commas

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,19 @@ let pokemonList = [];
 pokemonList = [
     {
         name: 'Pikachu',
-        height: 0.4 //meters, 
+        height: 0.4, //meters 
         type: ['Electric'],
         species: 'Mouse'
     },
     {
         name: 'Charizard',
-        height: 1.7 //meters,
+        height: 1.7, //meters
         type: ['Fire', 'Flying'],
         species: 'Flame'
     },
     {
         name: 'Mew',
-        height: 0.4 //meters,
+        height: 0.4, //meters
         type: ['Psychic'],
         species: 'New Species'
     }
