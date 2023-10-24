1ae322caf51cf121404c0140739d1cbc56c8ccbb
Matthew <106117475+MLindbloom@users.noreply.github.com>
2023-10-24 10:57:02 -0700

    Changed loop to forEach funtion

--- js/scripts.js
+++ js/scripts.js
@@ -21,9 +21,18 @@ pokemonList = [
     }
 ]
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.0)
-     {document.write(pokemonList[i].name + "  " + "(height: " + "  " +  pokemonList[i].height + ") " + "- Wow, that\'s big!"+ "<br>"); //if pokemon's height is over 1.0m print "Wow, that's big!"
-  } else {
-  document.write(pokemonList[i].name + "  " + "(height: " + "  " +  pokemonList[i].height + ") " + "<br>");}
-  }
+// for (let i = 0; i < pokemonList.length; i++) {
+//     if (pokemonList[i].height > 1.0)
+//      {document.write(pokemonList[i].name + "  " + "(height: " + "  " +  pokemonList[i].height + ") " + "- Wow, that\'s big!"+ "<br>"); //if pokemon's height is over 1.0m print "Wow, that's big!"
+//   } else {
+//   document.write(pokemonList[i].name + "  " + "(height: " + "  " +  pokemonList[i].height + ") " + "<br>");}
+//   }
+
+pokemonList.forEach(function(pokemon) {
+    if (pokemon.height > 1.0) {
+        document.write(pokemon.name + '  ' + '(height:' + '  ' + pokemon.height + 'm) ' + '- Wow, that\'s big!' + '<br>'); //if pokemon's height is over 1.0m print "Wow, that's big!"
+    }
+    else {
+        document.write(pokemon.name + '  ' + '(height:' + '  ' + pokemon.height + 'm) ' + '<br>');
+    }
+});
